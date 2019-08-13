6b90a58ba13e2ea1324d37d7aba3cae151fcd1d0
Ned Bellavance <ned.bellavance@gmail.com>
2019-08-13 16:52:04 -0400

    try again?

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ app.get('/', function(req, res){
  app.set('view engine', 'pug');
  app.set('views', './views');
  
- // for parsing application/json
- app.use(bodyParser.json()); 
- 
- // for parsing application/xwww-
- app.use(bodyParser.urlencoded({ extended: true })); 
- //form-urlencoded
- 
  // for parsing multipart/form-data
  app.use(upload.array()); 
  app.use(express.static('public'));
@@ -36,4 +29,4 @@ app.get('/', function(req, res){
     res.send("Your secret is: " + secret);
  });
 
- app.listen(443);
+ module.exports = app;
