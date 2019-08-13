05bfbfe77bd32994c010fd79df39be36735ef216
Ned Bellavance <ned.bellavance@gmail.com>
2019-08-13 16:57:34 -0400

    fix semi colon

--- index.js
+++ index.js
@@ -30,4 +30,4 @@ app.get('/', function(req, res){
  });
 
  const port = process.env.PORT || 1337;
- app.listen(port, () => console.log("Server is running on port" + port);)
+ app.listen(port, () => console.log("Server is running on port" + port));
