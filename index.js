3bccc061eceaf2601dfadd9505bd7460c2f4a883
Ned Bellavance <ned.bellavance@gmail.com>
2019-08-15 10:38:26 -0400

    use new keyvault-secrets library

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
 var app = express();
+import {SecretClient} from "@azure/keyvault-secrets";
 
 app.get('/', function(req, res){
     res.render('form');
@@ -19,7 +20,7 @@ app.get('/', function(req, res){
  
  app.post('/', function(req, res){
    console.log(req.body);
-   const KeyVault = require('azure-keyvault');
+   //const KeyVault = require('azure-keyvault');
    const msRestAzure = require('ms-rest-azure');
    msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'}, function(err, credentials){
       if(err){
@@ -34,11 +35,15 @@ app.get('/', function(req, res){
          var vaultUri = "https://" + req.body.keyVault + ".vault.azure.net/";
          console.log("vaultUri is " + vaultUri);
          console.log("secret name is " + req.body.secretName);
-         var keyVaultClient = new KeyVault.KeyVaultClient(credentials);
-         keyVaultClient.getSecret(vaultUri, req.body.secretName, "").then((result) => {
-            console.log("result is " + result);
-            res.send("Your secret is: " + result);
-         })
+         //var keyVaultClient = new KeyVault.KeyVaultClient(credentials);
+         const client = new SecretClient(vaultUri, credentials);
+         var secret = await client.getSecret(req.body.secretName);
+         console.log("result is " + secret);
+         res.send("Your secret is: " + secret);
+         //keyVaultClient.getSecret(vaultUri, req.body.secretName, "").then((result) => {
+         //   console.log("result is " + result);
+         //   res.send("Your secret is: " + result);
+         //})
       }
    });
    
